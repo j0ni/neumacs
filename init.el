716ced673a73a4828f0f59eb734b4dfd7be00e9d
J Irving <j@lollyshouse.ca>
2020-12-23 19:20:56 +0100

    Old code from the days of yore

--- init.el
+++ init.el
@@ -111,6 +111,34 @@ If BACKWARD-ONLY is non-nil, only delete them before point."
   (defun j0ni/reload-config ()
     (interactive)
     (load-file (expand-file-name "init.el" user-emacs-directory)))
+  ;; Don't know where I found this, but it I didn't write it
+  (defun j0ni/toggle-window-split ()
+    "Vertical split shows more of each line, horizontal split shows
+more lines. This code toggles between them. It only works for
+frames with exactly two windows."
+    (interactive)
+    (if (= (count-windows) 2)
+        (let* ((this-win-buffer (window-buffer))
+               (next-win-buffer (window-buffer (next-window)))
+               (this-win-edges (window-edges (selected-window)))
+               (next-win-edges (window-edges (next-window)))
+               (this-win-2nd (not (and (<= (car this-win-edges)
+                                           (car next-win-edges))
+                                       (<= (cadr this-win-edges)
+                                           (cadr next-win-edges)))))
+               (splitter
+                (if (= (car this-win-edges)
+                       (car (window-edges (next-window))))
+                    'split-window-horizontally
+                  'split-window-vertically)))
+          (delete-other-windows)
+          (let ((first-win (selected-window)))
+            (funcall splitter)
+            (if this-win-2nd (other-window 1))
+            (set-window-buffer (selected-window) this-win-buffer)
+            (set-window-buffer (next-window) next-win-buffer)
+            (select-window first-win)
+            (if this-win-2nd (other-window 1))))))
   :config
   (when window-system
     (fringe-mode 8)
@@ -131,7 +159,8 @@ If BACKWARD-ONLY is non-nil, only delete them before point."
   :bind (("M-[" . beginning-of-buffer)
          ("M-]" . end-of-buffer)
          ("C-c ." . j0ni/delete-whitespace)
-         ("C-c s" . j0ni/insert-shrug)))
+         ("C-c s" . j0ni/insert-shrug)
+         ("C-x |" . j0ni/toggle-window-split)))
 
 (use-package exec-path-from-shell
   :init
