fa8fcacbd15bdc005061692b3724b08c951e7f5e
J Irving <j@lollyshouse.ca>
2020-12-26 16:30:52 +0100

    Use ibuffer-vc for layout and ordering

--- init.el
+++ init.el
@@ -195,6 +195,27 @@ frames with exactly two windows."
   (load-theme 'modus-vivendi t)
   (set-face-attribute 'bold nil :weight 'semibold))
 
+(use-package ibuffer-vc
+  :hook ((ibuffer-hook . j0ni/ibuffer-vc-hook))
+  :custom
+  (ibuffer-formats
+   '((mark modified read-only vc-status-mini " "
+           (name 18 18 :left :elide)
+           " "
+           (size 9 -1 :right)
+           " "
+           (mode 16 16 :left :elide)
+           " "
+           (vc-status 16 16 :left)
+           " "
+           vc-relative-file)))
+  :init
+  (defun j0ni/ibuffer-vc-hook ()
+    (ibuffer-auto-mode 1)
+    (ibuffer-vc-set-filter-groups-by-vc-root)
+    (unless (eq ibuffer-sorting-mode 'alphabetic)
+      (ibuffer-do-sort-by-alphabetic))))
+
 (use-package rainbow-mode)
 (use-package rainbow-delimiters)
 
