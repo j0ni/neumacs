6c3ef670cf9490d800d9b8a784edd6f50e34df27
J Irving <j@lollyshouse.ca>
2021-01-19 12:21:45 +0100

    Some boot improvements from prot

--- lisp/boot.el
+++ lisp/boot.el
@@ -30,5 +30,20 @@
 (straight-use-package 'diminish)
 (straight-use-package 'use-package)
 
+;; Configure `use-package' prior to loading it.
+(eval-and-compile
+  (setq use-package-always-ensure nil)  ; ESSENTIAL for `straight.el'
+  (setq use-package-always-defer nil)
+  (setq use-package-always-demand nil)
+  (setq use-package-expand-minimally nil)
+  (setq use-package-enable-imenu-support t)
+  (setq use-package-compute-statistics nil)
+  ;; The following is VERY IMPORTANT.  Write hooks using their real name
+  ;; instead of a shorter version: after-init ==> `after-init-hook'.
+  ;;
+  ;; This is to empower help commands with their contextual awareness,
+  ;; such as `describe-symbol'.
+  (setq use-package-hook-name-suffix nil))
+
 (provide 'boot)
 ;;; boot.el ends here
