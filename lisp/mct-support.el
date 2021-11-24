4a7368d7cccc7b899f81902407debc6885d1522f
J Irving <j@lollyshouse.ca>
2021-11-24 12:41:04 -0500

    mct tweaks, still not quite right

--- lisp/mct-support.el
+++ lisp/mct-support.el
@@ -5,23 +5,17 @@
 (use-package mct
   :straight
   (mct.el :type git :host gitlab :repo "protesilaos/mct.el")
-  ;; (:type built-in)
-
-  ;; :load-path
-  ;; ("~/Scratch/emacs/mct")
-
-  :hook
-  ((after-init-hook . mct-mode))
 
   :init
   (setq mct-remove-shadowed-file-names t) ; works when `file-name-shadow-mode' is enabled
   (setq mct-hide-completion-mode-line nil)
   (setq mct-show-completion-line-numbers nil)
-  (setq mct-apply-completion-stripes t)
+  (setq mct-apply-completion-stripes nil)
 
   (setq mct-live-completion 'visible)
-  (setq mct-minimum-input 3)
-  (setq mct-live-update-delay 0.1)
+  ;; this isn't used if mct-live-completion is not set to
+  ;; (setq mct-minimum-input 3)
+  (setq mct-live-update-delay 0)
 
   ;; NOTE: setting this variable with `setq', requires `mct-mode' to be
   ;; reloaded.
@@ -43,23 +37,24 @@
   ;; completions' buffer.
   (setq mct-completion-passlist
         '(imenu
+          consult-imenu
+          consult-line
           Info-goto-node
           Info-index
           Info-menu
           vc-retrieve-tag
-          projectile-switch-project
+          ;;projectile-switch-project
           ;; consult-buffer
           ))
 
   ;; You can place the Completions' buffer wherever you want, by following
   ;; the syntax of `display-buffer'.  For example, try this:
 
-  ;; (setq mct-display-buffer-action
-  ;;       (quote ((display-buffer-reuse-window
-  ;;                display-buffer-in-side-window)
-  ;;               (side . left)
-  ;;               (slot . 99)
-  ;;               (window-width . 0.3))))
-  )
+  (setq mct-display-buffer-action
+        '((display-buffer-reuse-window
+           display-buffer-at-bottom)
+          (window-height . 0.3)))
+
+  (mct-mode 1))
 
 (provide 'mct-support)
