05484c2d0bfbdb66531dfb5a2edd6358184656e9
J Irving <j@lollyshouse.ca>
2021-12-30 23:54:49 +0100

    Missed a couple of coding systems :)

--- init.el
+++ init.el
@@ -85,6 +85,8 @@
 (set-language-environment "UTF-8")
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(set-file-name-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
 ;;; Fonty fonty fonty fonty fonty LEAVE ME ALONE fonty fonty fonty
