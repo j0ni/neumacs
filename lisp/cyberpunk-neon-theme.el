b17f48578c5bfc5463e92a1435d01ae0563c7d19
J Irving <j@lollyshouse.ca>
2022-05-25 16:30:27 -0400

    Vendor a theme that isn't on melpa yet

---
+++ lisp/cyberpunk-neon-theme.el
@@ -0,0 +1,135 @@
+;;; cyberpunk-neon-theme.el --- Custom face theme for Emacs
+
+;; This theme was generated with vimco.el
+;; You can get it from:
+;; <https://github.com/UwUnyaa/vimco>
+
+;;; Code:
+
+(deftheme cyberpunk-neon)
+
+(custom-theme-set-faces 'cyberpunk-neon
+                        '(underline
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline t))))
+                        '(font-lock-type-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:weight bold))))
+                        '(font-lock-string-face
+                          ((((class color)
+                             (min-colors 89))
+                            nil)))
+                        '(font-lock-builtin-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:weight bold))))
+                        '(font-lock-preprocessor-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:weight bold))))
+                        '(font-lock-keyword-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:weight bold))))
+                        '(font-lock-variable-name-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:weight bold))))
+                        '(font-lock-function-name-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:weight bold))))
+                        '(font-lock-warning-face
+                          ((((class color)
+                             (min-colors 89))
+                            nil)))
+                        '(cursor
+                          ((((class color)
+                             (min-colors 89))
+                            nil)))
+                        '(font-lock-constant-face
+                          ((((class color)
+                             (min-colors 89))
+                            nil)))
+                        '(font-lock-doc-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:weight bold))))
+                        '(font-lock-comment-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:weight bold))))
+                        '(show-paren-match-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :background "Cyan"))))
+                        '(default
+                           ((((class color)
+                              (min-colors 89))
+                             nil)))
+                        '(highline-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline t :background "Grey90"))))
+                        '(ac-selection-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:weight bold :background "Grey"))))
+                        '(ac-candidate-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:background "LightMagenta"))))
+                        '(flyspell-duplicate
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline
+                             (:style wave)))))
+                        '(flyspell-incorrect
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline
+                             (:style wave)))))
+                        '(region
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :background "LightGrey"))))
+                        '(mode-line-inactive
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t))))
+                        '(mode-line-buffer-id
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :weight bold))))
+                        '(mode-line
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :weight bold))))
+                        '(fringe
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline t :foreground "Brown"))))
+                        '(linum
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline t :foreground "Brown"))))
+                        '(isearch
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :background "Yellow"))))
+                        '(isearch-lazy-highlight-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t))))
+                        '(dired-directory
+                          ((((class color)
+                             (min-colors 89))
+                            (:weight bold :foreground "Blue")))))
+
+(provide-theme 'cyberpunk-neon)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
